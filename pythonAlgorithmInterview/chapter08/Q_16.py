c613addadad3b65b94f0a5cdf3bb75b5c8301013
kseon0828 <kseon0828@gmail.com>
2022-09-18 22:26:21 +0900

    chapter08) 16 풀이 추가

--- pythonAlgorithmInterview/chapter08/Q_16.py
+++ pythonAlgorithmInterview/chapter08/Q_16.py
@@ -38,3 +38,24 @@ def addTwoNumbers(self, l1, l2):
     resultStr = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
 
     return self.toReversedLinkedList(str(resultStr))
+
+###전가산기 구현
+def addTwoNumbers(l1:ListNode, l2:ListNode):
+    root = head = ListNode(0)
+
+    carry = 0
+    while l1 or l2 or carry:
+        sum = 0
+        if l1:
+            sum += l1.val
+            l1 = l1.next
+        if l2:
+            sum +=l2.val
+            l2 = l2.next
+
+        carry, val = divmod(sum + carry, 10)
+        #ListNode(val)를 head의 next로 연결
+        head.next = ListNode(val)
+        #head.next를 head로 갱신
+        head = head.next
+    return root.next
