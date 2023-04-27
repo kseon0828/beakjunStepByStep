02b768158011cccea3174921c89226e2df3c25a3
kseon0828 <kseon0828@gmail.com>
2023-04-28 00:08:31 +0900

    [BOJ] Q1436 : 영화감독 숌

---
+++ class1/Project1/p1436.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n, result, temp;
+	int cnt = 0; //현재 몇번째 순서인지
+
+	cin >> n;
+
+	//현재 순서가 n이 아닐때 까지 진행
+	while (cnt != n) {
+		result++;
+		temp = result;
+
+		//temp를 통해 현재 수가 n번째 종말 수인지 판단
+		//temp가 세자리수가 안되면 안쪽 while문 탈출
+		while (temp != 0) {
+			//종말 수라면 cnt를 증가시키고 안쪽 while문 탈출
+			if (temp % 1000 == 666) {
+				cnt++;
+				break;
+			}
+			//뒤에 세자리가 종말 수가 아니라면, 뒤에 수를 뺌
+			else temp /= 10;
+		}
+	}
+
+
+	return 0;
+}
