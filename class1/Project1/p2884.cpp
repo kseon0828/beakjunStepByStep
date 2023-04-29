2a6d34f1347ae5c19efb1465aae8a3e4ea6c8316
kseon0828 <kseon0828@gmail.com>
2023-04-29 20:39:40 +0900

    [BOJ] Q2884 : 알람 시계

---
+++ class1/Project1/p2884.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int h, m;
+
+	cin >> h >> m;
+
+	if (m >= 45) {
+		cout << h << " " << m - 45;
+	}
+	else if (h != 0) {
+		cout << h - 1 << " " << 60 + (m - 45);
+	}
+	else {
+		cout << 23 << " " << 60 + (m - 45);
+	}
+
+	return 0;
+}
