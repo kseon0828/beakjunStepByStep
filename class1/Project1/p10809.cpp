f010582036ea2c627c90582192134182f6ceced4
kseon0828 <kseon0828@gmail.com>
2023-05-01 14:25:50 +0900

    [BOJ] Q10809 : 알파벳 찾기

---
+++ class1/Project1/p10809.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	string s;
+	int charCnt[26] = { -1 };
+	fill(charCnt, charCnt + 26, -1);
+
+	cin >> s;
+
+	for (int i = 0; i < s.length(); i++) {
+		if (charCnt[(int)s[i] - 'a'] == -1) {
+			charCnt[(int)s[i] - 'a'] = i;
+		}
+		else continue;
+	}
+
+	for (int i : charCnt) cout << i << ' ';
+
+
+	return 0;
+}
